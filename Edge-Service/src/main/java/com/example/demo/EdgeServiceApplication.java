faa12a56d5053d23c82d5df1fdab916cf92d8ff4
Aditya Rawat <aditya.rawat@hotmail.com>
2021-02-10 05:45:00 +0530

    github pages commit

---
+++ Edge-Service/src/main/java/com/example/demo/EdgeServiceApplication.java
@@ -0,0 +1,80 @@
+package com.example.demo;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@EnableFeignClients("com.example.demo")
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class EdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServiceApplication.class, args);
+	}
+}
+
+@Data
+class Item {
+    private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+@FeignClient(name = "item-catalog-service", url="localhost:8088")
+interface ItemClient {
+
+    @GetMapping("/items")
+    Resources<Item> readItems();
+}
+
+@RestController
+class GoodItemApiAdapterRestController {
+
+    private final ItemClient itemClient;
+
+    public GoodItemApiAdapterRestController(ItemClient itemClient) {
+        this.itemClient = itemClient;
+    }
+
+    public Collection<Item> fallback() {
+    	return new ArrayList<>();
+    }
+    
+    @HystrixCommand(fallbackMethod = "fallback")
+    @GetMapping("/top-brands")
+    @CrossOrigin(origins = "*")
+    public Collection<Item> goodItems() {
+        return itemClient.readItems()
+                .getContent()
+                .stream()
+                .filter(this::isGreat)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isGreat(Item item) {
+        return !item.getName().equals("Nike") &&
+                !item.getName().equals("Adidas") &&
+                !item.getName().equals("Reebok");
+    }
+}
